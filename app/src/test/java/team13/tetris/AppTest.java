e030fb6b936cfcfe0b3cabb472e67af2848e03c4
JunsuSeo <junesue02@naver.com>
2025-10-27 22:37:14 +0900

    test: Create SceneManagerTest

---
+++ app/src/test/java/team13/tetris/AppTest.java
@@ -0,0 +1,9 @@
+package team13.tetris;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("App 테스트")
+public class AppTest {
+  // App 클래스는 JavaFX Application을 확장하므로, 단위 테스트가 제한적이다.
+  // 주요 기능은 통합 테스트 또는 수동 테스트로 검증하는 것이 적합하다.
+}
